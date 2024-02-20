58f465d917a800822631f1536899d3553eeae665
tichagodwill1 <tichagodwill1@gmail.com>
2024-02-20 18:11:51 +0300

    handling user errors

--- script.js
+++ script.js
@@ -17,16 +17,23 @@ async function fetchData(city) {
   try {
     const res = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e4a932663c53c2716b0c3c5662284934&units=metric`);
+      if (res.status == 404) {
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+      }else{
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+      }
     const data = await res.json();
     console.log(data);
 
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     // document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     // document.querySelector(".wind").innerHTML = data.wind.speed + "Km/hr";
-    cityElement.textContent = "Weather in " + data.name; 
+    cityElement.textContent = "Temperature in " + data.name; 
     humidityElement.textContent = "Humidity  " + data.main.humidity + "%";
     windElement.textContent = "Wind Speed  " + data.wind.speed + "Km/hr";
-    desciptionElement.textContent = data.weather[0].description;
+    desciptionElement.textContent = data.weather[0].description.toUpperCase();
 
 // if(data.weather[0].main == "Clouds"){
 //   imageIcons.src = "https://openweathermap.org/img/wn/01n.png"
